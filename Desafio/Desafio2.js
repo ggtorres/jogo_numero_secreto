b2467e86757df6db83b6b0e520a628543d2dd5a3
Guilherme Torres <80927835+ggtorres@users.noreply.github.com>
2023-11-05 12:12:53 -0300

    Add files via upload

---
+++ Desafio/Desafio2.js
@@ -0,0 +1,81 @@
+/*
+function calculaIMC(altura, peso){
+
+  let imc = peso / (alturaMetros * alturaMetros);
+}
+*/
+
+/*
+function calcularFatorial(numero) {
+  if (numero === 0 || numero === 1) {
+    return 1;
+  }
+
+  let fatorial = 1;
+  for (let i = 2; i <= numero; i++) {
+    fatorial *= i;
+  }
+
+  return fatorial;
+}
+
+// Exemplo de uso
+let numero = 5;
+let resultado = calcularFatorial(numero);
+console.log(`O fatorial de ${numero} é ${resultado}`);
+*/
+
+/*function converterDolarParaReal(valorEmDolar) {
+  let cotacaoDolar = 4.80;
+  let valorEmReais = valorEmDolar * cotacaoDolar;
+  return valorEmReais.toFixed(2);
+}
+
+// Exemplo de uso
+let valorEmDolar = 50;
+let valorEmReais = converterDolarParaReal(valorEmDolar);
+console.log(`${valorEmDolar} dólares equivalem a R$ ${valorEmReais}`);
+*/
+
+/*
+function calcularAreaPerimetroSalaRetangular(altura, largura) {
+  let area = altura * largura;
+  let perimetro = 2 * (altura + largura);
+  
+  console.log(`Área da sala: ${area} metros quadrados`);
+  console.log(`Perímetro da sala: ${perimetro} metros`);
+}
+
+// Exemplo de uso
+let altura = 3; // em metros
+let largura = 5; // em metros
+calcularAreaPerimetroSalaRetangular(altura, largura);
+
+*/
+
+/*
+function calcularAreaPerimetroSalaCircular(raio) {
+  let area = Math.PI * raio * raio;
+  let perimetro = 2 * Math.PI * raio;
+  
+  console.log(`Área da sala circular: ${area.toFixed(2)} metros quadrados`);
+  console.log(`Perímetro da sala circular: ${perimetro.toFixed(2)} metros`);
+}
+
+// Exemplo de uso
+let raio = 4; // em metros
+calcularAreaPerimetroSalaCircular(raio);
+*/
+
+/*
+function mostrarTabuada(numero) {
+  for (let i = 1; i <= 10; i++) {
+    let resultado = numero * i;
+    console.log(`${numero} x ${i} = ${resultado}`);
+  }
+}
+
+// Exemplo de uso
+let numero = 7;
+mostrarTabuada(numero);
+*/
---
+++ Desafio/Desafio3.js
@@ -0,0 +1,28 @@
+/*
+let listaGenerica = [];
+*/
+
+/*
+let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+*/
+
+/*
+let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+linguagensDeProgramacao.push('Java', 'Ruby', 'GoLang');
+// Lista ao final: ['JavaScript', 'C', 'C++', 'Kotlin', 'Python', 'Java', 'Ruby', 'GoLang']
+*/
+
+/*
+nomes = ['JavaScript', 'Python', 'Go'];
+console.log(nomes[0]);
+*/
+
+/*
+nomes = ['JavaScript', 'Python', 'Go'];
+console.log(nomes[1]);
+*/
+
+/*
+nomes = ['JavaScript', 'Python', 'Go'];
+console.log(nomes[2]);
+*/
